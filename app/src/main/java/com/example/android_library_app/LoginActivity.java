d0092cc49dd01f46353be48dac8ad74e901d800e
Rethima-Reddy <rethima.wf37@gmail.com>
2020-02-29 04:18:25 -0600

    updaetd some content on login page

--- app/src/main/java/com/example/android_library_app/LoginActivity.java
+++ app/src/main/java/com/example/android_library_app/LoginActivity.java
@@ -7,16 +7,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText usernameET, passwordET;
+    TextView registerTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // register screen intent.
+        registerTV = findViewById(R.id.registerTV);
+        registerTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,RegistrationScreen.class);
+                startActivityForResult(intent,1);
+            }
+        });
     }
+
+
+
 
     // will triggered back to main activity with login is success
     // if user not authorized stay in this activity only (don't call intent logic)
