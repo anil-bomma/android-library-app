48c8833813ed0bbff6be529828cfb1431515809e
bomma.anil <anil.bommaa@gmail.com>
2020-02-29 00:59:57 -0600

    added login activity java file

---
+++ app/src/main/java/com/example/android_library_app/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.android_library_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText username, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    // will triggered back to main activity with login is success
+    // if user not authorized stay in this activity only (don't call intent logic)
+    public void authenticateUsernamePassword(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("role", "admin");
+        startActivity(intent);
+        finish();
+    }
+}
