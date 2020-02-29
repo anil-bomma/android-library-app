10da4322d44aec3d40a43ae455c7d2c806491c77
bomma.anil <anil.bommaa@gmail.com>
2020-02-29 00:57:02 -0600

    updated main activity

--- app/src/main/java/com/example/android_library_app/MainActivity.java
+++ app/src/main/java/com/example/android_library_app/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.android_library_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get the role from login intent and trigger to new intent based on role
+        Intent intent = getIntent();
+        String userRole = intent.getStringExtra("role");
+        TextView homeId = findViewById(R.id.homeId);
+        homeId.setText(("userRole: " + userRole));
     }
+
+
 }
