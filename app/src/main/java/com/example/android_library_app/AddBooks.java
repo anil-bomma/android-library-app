00ee84e5077cae0114e552136ccb4254f9acce20
Mahender <mahender.lfl95@gmail.com>
2020-03-02 03:29:39 -0600

    modified AddBooks activity's java file and added few fields

--- app/src/main/java/com/example/android_library_app/AddBooks.java
+++ app/src/main/java/com/example/android_library_app/AddBooks.java
@@ -3,12 +3,25 @@ package com.example.android_library_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class AddBooks extends AppCompatActivity {
+
+    Button add_booksBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_books);
+
+        add_booksBTN = findViewById(R.id.add_booksBTN);
+        add_booksBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AddBooks.this, "Book Added to the database", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
