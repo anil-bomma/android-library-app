475763dcfff4de3d9df9e0003912050efe455d57
Deepthi1003 <dtchokka@gmail.com>
2020-03-01 22:14:02 -0600

    Modified the DepartmentAdminScreen Activity

--- app/src/main/java/com/example/android_library_app/DepartmentAdminScreen.java
+++ app/src/main/java/com/example/android_library_app/DepartmentAdminScreen.java
@@ -2,13 +2,29 @@ package com.example.android_library_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class DepartmentAdminScreen extends AppCompatActivity {
+
+    Button deptAdmin_listBTN,deptAdmin_addBookBTN,deptAdmin_profileBTN,deptAdmin_settingsBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_department_admin_screen);
+
+         deptAdmin_listBTN = findViewById(R.id.deptAdmin_listBTN);
+        deptAdmin_listBTN.setOnClickListener(new OnClickListener(){
+        @Override
+        public void onClick (View v){
+        Intent intent = new Intent(DepartmentAdminScreen.this, ListOfBooks.class);
+        startActivity(intent);
+        }
+        });
     }
+
 }
