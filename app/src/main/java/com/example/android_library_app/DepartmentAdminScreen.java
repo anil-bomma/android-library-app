c1066d303c7ea34b86fab3b8ad6a834c3f855bfd
Deepthi1003 <dtchokka@gmail.com>
2020-03-01 22:52:37 -0600

    Modified the DepartmentAdminScreen Activity

--- app/src/main/java/com/example/android_library_app/DepartmentAdminScreen.java
+++ app/src/main/java/com/example/android_library_app/DepartmentAdminScreen.java
@@ -17,13 +17,40 @@ public class DepartmentAdminScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_department_admin_screen);
 
-         deptAdmin_listBTN = findViewById(R.id.deptAdmin_listBTN);
+        deptAdmin_listBTN = findViewById(R.id.deptAdmin_listBTN);
         deptAdmin_listBTN.setOnClickListener(new OnClickListener(){
         @Override
         public void onClick (View v){
         Intent intent = new Intent(DepartmentAdminScreen.this, ListOfBooks.class);
         startActivity(intent);
         }
+        });
+
+        deptAdmin_addBookBTN = findViewById(R.id.deptAdmin_addBookBTN);
+        deptAdmin_addBookBTN.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick (View v){
+                Intent intent = new Intent(DepartmentAdminScreen.this, AddBooks.class);
+                startActivity(intent);
+            }
+        });
+
+        deptAdmin_profileBTN = findViewById(R.id.deptAdmin_profileBTN);
+        deptAdmin_profileBTN.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick (View v){
+                Intent intent = new Intent(DepartmentAdminScreen.this, UserProfile.class);
+                startActivity(intent);
+            }
+        });
+
+        deptAdmin_settingsBTN = findViewById(R.id.deptAdmin_settingsBTN);
+        deptAdmin_settingsBTN.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick (View v){
+                Intent intent = new Intent(DepartmentAdminScreen.this, Settings.class);
+                startActivity(intent);
+            }
         });
     }
 
