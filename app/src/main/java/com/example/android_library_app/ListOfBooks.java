9be1e2f4f9c8ac92708f7c3e30d42021dbcd0351
Deepthi1003 <dtchokka@gmail.com>
2020-03-01 23:21:35 -0600

    Created an activity named List of books activity both XML and java files

---
+++ app/src/main/java/com/example/android_library_app/ListOfBooks.java
@@ -0,0 +1,14 @@
+package com.example.android_library_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListOfBooks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_books);
+    }
+}
