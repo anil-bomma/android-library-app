1343b116b79e453c077a1475b7e5e53ce153e664
Deepthi1003 <dtchokka@gmail.com>
2020-03-01 22:36:38 -0600

    Modified the AdminScreen Activity

--- app/src/main/java/com/example/android_library_app/AdminScreen.java
+++ app/src/main/java/com/example/android_library_app/AdminScreen.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class AdminScreen extends AppCompatActivity {
 
-    Button  admin_addBookBTN, admin_deptAdminListBTN,
+    Button  admin_listBTN, admin_addBookBTN, admin_deptAdminListBTN,
             admin_addOrRemoveDeptBTN, admin_profileBTN, admin_settingsBTN;
 
     @Override
@@ -18,13 +18,68 @@ public class AdminScreen extends AppCompatActivity {
         setContentView(R.layout.activity_admin_screen);
 
 
-        Button admin_listBTN = findViewById(R.id.admin_listBTN);
+        admin_listBTN = findViewById(R.id.admin_listBTN);
         admin_listBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(AdminScreen.this,ListOfBooks.class);
                 startActivity(intent);
             }
+        });
+
+        admin_addBookBTN = findViewById(R.id.admin_addBookBTN);
+        admin_addBookBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminScreen.this,AddBooks.class);
+                startActivity(intent);
+
+            }
+
+    });
+
+        admin_addOrRemoveDeptBTN = findViewById(R.id.admin_addOrRemoveDeptBTN);
+        admin_addOrRemoveDeptBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminScreen.this,AddRemoveDeptAdmin.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+        admin_deptAdminListBTN = findViewById(R.id.admin_deptAdminListBTN);
+        admin_deptAdminListBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminScreen.this,ListDeptAdmin.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+        admin_profileBTN = findViewById(R.id.admin_profileBTN);
+        admin_profileBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminScreen.this,UserProfile.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+        admin_settingsBTN = findViewById(R.id.admin_settingsBTN);
+        admin_addOrRemoveDeptBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminScreen.this,Settings.class);
+                startActivity(intent);
+
+            }
+
         });
     }
 
