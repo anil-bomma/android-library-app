87c88a30f9ce8181944b050960285543dfd1bf92
Deepthi1003 <dtchokka@gmail.com>
2020-03-01 20:18:07 -0600

    Modified the AdminScreen Activity

--- app/src/main/java/com/example/android_library_app/AdminScreen.java
+++ app/src/main/java/com/example/android_library_app/AdminScreen.java
@@ -2,13 +2,31 @@ package com.example.android_library_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminScreen extends AppCompatActivity {
+
+    Button  admin_addBookBTN, admin_deptAdminListBTN,
+            admin_addOrRemoveDeptBTN, admin_profileBTN, admin_settingsBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_screen);
+
+
+        Button admin_listBTN = findViewById(R.id.admin_listBTN);
+        admin_listBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminScreen.this,ListOfBooks.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
