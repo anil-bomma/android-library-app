c9c4232c33cc9392336206d3ce05c974fffd48b0
Deepthi1003 <dtchokka@gmail.com>
2020-03-02 18:13:46 -0600

    added search book code and layout

--- app/src/main/java/com/example/android_library_app/SearchBookFragment.java
+++ app/src/main/java/com/example/android_library_app/SearchBookFragment.java
@@ -4,16 +4,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class SearchBookFragment extends Fragment {
+    Button searchBTN;
+    EditText searchBookET;
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search_book, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search_book,container,false);
+
+        searchBTN = view.findViewById(R.id.searchBTN);
+        searchBookET = view.findViewById(R.id.searchBookET);
+        searchBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchBookET.getText();
+            }
+        });
+        return view;
     }
 }
