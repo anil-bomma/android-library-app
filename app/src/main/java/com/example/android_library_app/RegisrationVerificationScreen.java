a09ba19fcc127cf2b5a4e42cf5b7e589468b51fc
bomma.anil <anil.bommaa@gmail.com>
2020-03-09 03:41:39 -0500

    added register backend logic

--- app/src/main/java/com/example/android_library_app/RegisrationVerificationScreen.java
+++ app/src/main/java/com/example/android_library_app/RegisrationVerificationScreen.java
@@ -1,13 +1,25 @@
 package com.example.android_library_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisrationVerificationScreen extends AppCompatActivity {
 
@@ -17,6 +29,11 @@ public class RegisrationVerificationScreen extends AppCompatActivity {
     private static String emailId;
     private static String password;
     private static String vCode;
+
+    private FirebaseAuth fAuth;
+    private boolean authSuccess = false;
+
+    String userID;
 
     EditText verifyET;
     Button verifyBTN;
@@ -38,6 +55,9 @@ public class RegisrationVerificationScreen extends AppCompatActivity {
         // we will validate the user in this method
         verifyBTN = findViewById(R.id.verifyBTN);
         verifyET = findViewById(R.id.verifyET);
+
+        fAuth = FirebaseAuth.getInstance();
+
         verifyBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,40 +72,70 @@ public class RegisrationVerificationScreen extends AppCompatActivity {
 
                 if (userVcode.equals(vCode)) {
                     // save in database logic here
-                    Toast.makeText(RegisrationVerificationScreen.this, "Registered Successfully", Toast.LENGTH_LONG).show();
 
-                    // sending user details through email
-                    String subject = "B.D. Owens Library, login successful";
-                    String message = String.format(
-                            "Hello %s %s, here is the login details:%n%n", firstname, lastname);
+                    // register the user in firebase
+                    fAuth.createUserWithEmailAndPassword(emailId, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        System.out.println("-----------User Registered Successfully------");
+                                        Toast.makeText(
+                                                RegisrationVerificationScreen.this,
+                                                "User Registered Successfully.",
+                                                Toast.LENGTH_LONG
+                                        ).show();
 
-                    message += "\t" + String.format("EmailId: %s%n", emailId);
-                    message += "\t" + String.format("Password: %s%n", password);
-                    message += "\t" + String.format("StudentId: %s%n", studentId);
-                    message += String.format("%nNote: This is an automated message to confirm that " +
-                            "your account has created successfully.%n");
-                    message += String.format("%nThank you, %nLibrary Team");
-
-
-                    // sending verification code to user and sending to next intent also
-                    GmailServer mailServer = new GmailServer(
-                            RegisrationVerificationScreen.this,
-                            emailId,
-                            subject,
-                            message
-                    );
-                    mailServer.execute();
-
-                    Intent intent = new Intent(RegisrationVerificationScreen.this, LoginActivity.class);
-                    startActivity(intent);
-                    finish();
+                                        // sending mail to user
+                                        sendMailToUser();
+                                        // sending to login intent
+                                        Intent intent = new Intent(
+                                                RegisrationVerificationScreen.this,
+                                                LoginActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        System.out.println("-----------User Registered Failed------" + task.getException().getMessage());
+                                        Toast.makeText(
+                                                RegisrationVerificationScreen.this,
+                                                "Error ! " + task.getException().getMessage(),
+                                                Toast.LENGTH_LONG
+                                        ).show();
+                                    }
+                                }
+                            });
                 } else {
                     // stay in the same activity
-                    Toast.makeText(RegisrationVerificationScreen.this, "Invalid verification code", Toast.LENGTH_LONG).show();
-
+                    Toast.makeText(
+                            RegisrationVerificationScreen.this,
+                            "Invalid verification code",
+                            Toast.LENGTH_LONG).show();
                 }
 
             }
         });
+    }
+
+    public void sendMailToUser() {
+        String subject = "B.D. Owens Library, login successful";
+        String message = String.format(
+                "Hello %s %s, here is the login details:%n%n", firstname, lastname);
+
+        message += "\t" + String.format("EmailId: %s%n", emailId);
+        message += "\t" + String.format("Password: %s%n", password);
+        message += "\t" + String.format("StudentId: %s%n", studentId);
+        message += String.format("%nNote: This is an automated message to confirm that " +
+                "your account has created successfully.%n");
+        message += String.format("%nThank you, %nLibrary Team");
+
+
+        // sending verification code to user and sending to next intent also
+        GmailServer mailServer = new GmailServer(
+                RegisrationVerificationScreen.this,
+                emailId,
+                subject,
+                message
+        );
+        mailServer.execute();
     }
 }
