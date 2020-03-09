9fb6f4877b73dd33462b76d2955fa06255f3e042
bomma.anil <anil.bommaa@gmail.com>
2020-03-09 03:41:13 -0500

    added login backend logic

--- app/src/main/java/com/example/android_library_app/LoginActivity.java
+++ app/src/main/java/com/example/android_library_app/LoginActivity.java
@@ -1,45 +1,91 @@
 package com.example.android_library_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    FirebaseAuth fAuth;
+
     EditText emailIdET, passwordET;
     TextView registerTV;
+    Button loginBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        emailIdET = findViewById(R.id.emailIdET);
+        passwordET = findViewById(R.id.passwordET);
+
         // register screen intent.
         registerTV = findViewById(R.id.registerTV);
         registerTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this,RegistrationScreen.
+                Intent intent = new Intent(LoginActivity.this, RegistrationScreen.
                         class);
                 startActivity(intent);
+            }
+        });
+
+        // login code
+        fAuth = FirebaseAuth.getInstance();
+        loginBTN = findViewById(R.id.loginBTN);
+        loginBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailIdET.getText().toString().trim();
+                String password = passwordET.getText().toString().trim();
+                authenticateUsernamePassword(email, password);
             }
         });
     }
 
 
-
     // will triggered back to main activity with login is success
     // if user not authorized stay in this activity only (don't call intent logic)
-    public void authenticateUsernamePassword(View v) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("role", "admin");
-        intent.putExtra("userId", "1234");
-        MainActivity.loginStatus = true;
-        startActivity(intent);
-        finish();
+    public void authenticateUsernamePassword(String email, String password) {
+
+        // authenticate the user
+        fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(
+                            LoginActivity.this,
+                            "Logged in Successfully",
+                            Toast.LENGTH_SHORT).show();
+                    System.out.println("-----login sucess------");
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra("role", "admin");
+                    intent.putExtra("userId", "1234");
+                    MainActivity.loginStatus = true;
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(
+                            LoginActivity.this,
+                            "Error ! " + task.getException().getMessage(),
+                            Toast.LENGTH_LONG).show();
+                    System.out.println("-----login failed------");
+                }
+            }
+        });
+
     }
 }
