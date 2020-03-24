85454da166d6f9e30402652e15561b95e5f2a8dd
Deepthi1003 <dtchokka@gmail.com>
2020-03-24 14:16:23 -0500

    Updated Contact-Us Fragment

--- app/src/main/java/com/example/android_library_app/ContactMenuFragment.java
+++ app/src/main/java/com/example/android_library_app/ContactMenuFragment.java
@@ -1,19 +1,58 @@
 package com.example.android_library_app;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import static java.net.Proxy.Type.HTTP;
+
 public class ContactMenuFragment extends Fragment {
+    Button mobileBTN,emailIDBTN;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+
+        final View view = inflater.inflate(R.layout.fragment_contact, container, false);
+
+        mobileBTN=view.findViewById(R.id.mobileBTN);
+
+        mobileBTN.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    Uri number = Uri.parse("tel:+16605280951");
+                    Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+                    startActivity(callIntent);
+        }
+        });
+
+        emailIDBTN=view.findViewById(R.id.emailIDBTN);
+
+//        emailIDBTN.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+//
+//                //emailIntent.setType(HTTP.toString());
+//                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"libraryowens@gmail.com"});
+//                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Email subject");
+//                emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message text");
+//                emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("content://path/to/email/attachment"));
+//                startActivity(emailIntent);
+//
+//            }
+//        });
+
+
+    return view;
     }
 }
