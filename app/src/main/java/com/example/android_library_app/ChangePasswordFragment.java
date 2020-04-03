91e56ad48cc1cbcd4be329773082d093f06123b0
Mahender <mahender.lfl95@gmail.com>
2020-04-03 01:39:22 -0500

    Added a new fragment file for change password page

---
+++ app/src/main/java/com/example/android_library_app/ChangePasswordFragment.java
@@ -0,0 +1,25 @@
+package com.example.android_library_app;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ChangePasswordFragment extends Fragment {
+    Button searchBTN;
+    EditText searchBookET;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.change_password_screen,container,false);
+        return view;
+    }
+}
