e3cd783822ce0dc437e20b0194f07a4325394d69
MahenderReddy Surkanti <mahenderreddy.s1166@gmail.com>
2020-04-02 22:06:32 -0500

    Test commit please don't consider it

--- app/src/main/java/com/example/android_library_app/UserScreen.java
+++ app/src/main/java/com/example/android_library_app/UserScreen.java
@@ -10,5 +10,6 @@ public class UserScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_screen);
+        System.out.println("--------Hi-------");
     }
 }
