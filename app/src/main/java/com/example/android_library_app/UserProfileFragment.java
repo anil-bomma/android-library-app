c1e79bcfa0f7a99e8aa8ae68983e93d23541c8e4
Mahender <mahender.lfl95@gmail.com>
2020-04-03 01:40:06 -0500

    Added new fragment file for UserProfile screen

---
+++ app/src/main/java/com/example/android_library_app/UserProfileFragment.java
@@ -0,0 +1,40 @@
+package com.example.android_library_app;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
+public class UserProfileFragment extends Fragment {
+    Button searchBTN;
+    EditText searchBookET;
+
+    private FirebaseFirestore db;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user_profile,container,false);
+
+
+        // Database code
+        db = FirebaseFirestore.getInstance();
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setTimestampsInSnapshotsEnabled(true)
+                .build();
+        db.setFirestoreSettings(settings);
+
+
+        return view;
+    }
+}
