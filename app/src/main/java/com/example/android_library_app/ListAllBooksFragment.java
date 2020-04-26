bf98ee67589b1ad6d7d5b1ff6e2f753eaeb1dea4
bomma.anil <anil.bommaa@gmail.com>
2020-04-26 16:45:39 -0500

    bug fixed in list all books fragment.java file

--- app/src/main/java/com/example/android_library_app/ListAllBooksFragment.java
+++ app/src/main/java/com/example/android_library_app/ListAllBooksFragment.java
@@ -22,6 +22,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,8 +39,7 @@ import java.util.Map;
 
 class BooksModel {
 
-    private FirebaseFirestore db;
-    public static BooksAdapter booksAdapter = new BooksAdapter();
+//    private FirebaseFirestore db;
 
     public static class BooksInfo {
         public String title;
@@ -65,61 +65,20 @@ class BooksModel {
 
     private static BooksModel singleton = null;
 
-    public static BooksModel getSingleton() {
+
+    public static BooksModel getSingletonWithDB(ArrayList<BooksInfo> booksArray) {
         if (singleton == null) {
-            singleton = new BooksModel();
+            singleton = new BooksModel(booksArray);
         }
         return singleton;
     }
 
     public ArrayList<BooksInfo> booksArray;
 
-    private BooksModel() {
-        booksArray = new ArrayList<>();
-        db = FirebaseFirestore.getInstance();
-        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
-                .setTimestampsInSnapshotsEnabled(true)
-                .build();
-        db.setFirestoreSettings(settings);
-        loadModel();
+    private BooksModel(ArrayList<BooksInfo> booksArray) {
+        this.booksArray = booksArray;
     }
 
-    public void loadModel() {
-        System.out.println("------------load model-----------");
-        db.collection("books").get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Map<String, Object> book = document.getData();
-                                booksArray.add(new BooksInfo(
-                                        book.get("title").toString(),
-                                        book.get("author").toString(),
-                                        book.get("genre").toString(),
-                                        book.get("description").toString(),
-                                        book.get("publisher").toString(),
-                                        book.get("language").toString(),
-                                        book.get("available").toString()
-                                ));
-                            }
-                            booksAdapter.notifyDataSetChanged();
-                        } else {
-                            Log.w("ListAllBooks", "Error getting documents.", task.getException());
-                        }
-                    }
-                });
-
-//        booksArray.add(new BooksInfo("C++", "ABC", "12c edition"));
-//        booksArray.add(new BooksInfo("JAVA", "DEF", "8th edition"));
-//        booksArray.add(new BooksInfo("Android", "GHI", "09 edition"));
-//        booksArray.add(new BooksInfo("Project management", "JKL", "12th edition"));
-//        booksArray.add(new BooksInfo("Spring frame works", "MNO", "8th edition"));
-//        booksArray.add(new BooksInfo("IOS", "PQR", "20th edition"));
-//        booksArray.add(new BooksInfo("Big data", "STU", "12 edition"));
-//        booksArray.add(new BooksInfo("Data Visualization", "VWX", "8th edition"));
-//        booksArray.add(new BooksInfo("Machine Learning", "XYZ", "21st edition"));
-    }
 }
 
 
@@ -136,9 +95,14 @@ class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BooksViewHolder> {
 
     BooksModel booksModel;
 
-    public BooksAdapter() {
-        super();
-        booksModel = BooksModel.getSingleton();
+//    public BooksAdapter() {
+//        super();
+//        booksModel = BooksModel.getSingleton();
+//    }
+
+    // parameterised constructor
+    public BooksAdapter(ArrayList<BooksModel.BooksInfo> booksArray) {
+        booksModel = BooksModel.getSingletonWithDB(booksArray);
     }
 
     @NonNull
@@ -169,6 +133,11 @@ class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BooksViewHolder> {
 
 public class ListAllBooksFragment extends Fragment {
 
+    // fire-store database
+    private FirebaseFirestore db;
+    private ArrayList<BooksModel.BooksInfo> booksArray;
+    ProgressBar loadBooksBar;
+
     // recycler view.
     private BooksAdapter booksAdapter = null;
     private RecyclerView booksRV = null;
@@ -182,44 +151,44 @@ public class ListAllBooksFragment extends Fragment {
 
         private Context context;
 
-        @Override
-        public boolean onSingleTapConfirmed(MotionEvent e) {
-            View view = booksRV.findChildViewUnder(e.getX(), e.getY());
-            if (view != null) {
-                RecyclerView.ViewHolder holder = booksRV.getChildViewHolder(view);
-
-                if (holder instanceof BooksAdapter.BooksViewHolder) {
-                    int position = holder.getAdapterPosition();
-
-                    //Obtaining the title of the book
-                    BooksModel myModel = BooksModel.getSingleton();
-                    bookTitle = myModel.booksArray.get(position).title;
-                    bookAuthor = myModel.booksArray.get(position).author;
-                    bookGenre = myModel.booksArray.get(position).genre;
-                    bookAvailable = myModel.booksArray.get(position).available;
-                    bookDescription = myModel.booksArray.get(position).description;
-                    bookPublisher = myModel.booksArray.get(position).publisher;
-                    bookLanguage = myModel.booksArray.get(position).language;
-
-
-                    Toast.makeText(getContext(), bookTitle, Toast.LENGTH_SHORT).show();
-
-                    FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-                    transaction.add(R.id.fragment_container, bookDescriptionFragment, "book description");
-                    transaction.addToBackStack(null);
-                    transaction.commit();
-
-                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            new BookDescriptionFragment()).addToBackStack(null).commit();
-
-
-                    // handling single tap.
-                    Log.d("click", "clicked on item " + position);
-                    return true;
-                }
-            }
-            return false;
-        }
+//        @Override
+//        public boolean onSingleTapConfirmed(MotionEvent e) {
+//            View view = booksRV.findChildViewUnder(e.getX(), e.getY());
+//            if (view != null) {
+//                RecyclerView.ViewHolder holder = booksRV.getChildViewHolder(view);
+//
+//                if (holder instanceof BooksAdapter.BooksViewHolder) {
+//                    int position = holder.getAdapterPosition();
+//
+//                    //Obtaining the title of the book
+//                    BooksModel myModel = BooksModel.getSingleton();
+//                    bookTitle = myModel.booksArray.get(position).title;
+//                    bookAuthor = myModel.booksArray.get(position).author;
+//                    bookGenre = myModel.booksArray.get(position).genre;
+//                    bookAvailable = myModel.booksArray.get(position).available;
+//                    bookDescription = myModel.booksArray.get(position).description;
+//                    bookPublisher = myModel.booksArray.get(position).publisher;
+//                    bookLanguage = myModel.booksArray.get(position).language;
+//
+//
+//                    Toast.makeText(getContext(), bookTitle, Toast.LENGTH_SHORT).show();
+//
+//                    FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+//                    transaction.add(R.id.fragment_container, bookDescriptionFragment, "book description");
+//                    transaction.addToBackStack(null);
+//                    transaction.commit();
+//
+//                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                            new BookDescriptionFragment()).addToBackStack(null).commit();
+//
+//
+//                    // handling single tap.
+//                    Log.d("click", "clicked on item " + position);
+//                    return true;
+//                }
+//            }
+//            return false;
+//        }
     }
 
     @Nullable
@@ -228,24 +197,63 @@ public class ListAllBooksFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
 
-        View view = inflater.inflate(R.layout.fragment_list_all_books, container, false);
+        final View view = inflater.inflate(R.layout.fragment_list_all_books, container, false);
 
-        // recycler view code.
-        booksAdapter = new BooksAdapter();
-        booksRV = view.findViewById(R.id.myRV);
-        booksRV.setAdapter(booksAdapter);
-        bookDescriptionFragment = new BookDescriptionFragment();
-        RecyclerView.LayoutManager myManager = new LinearLayoutManager(view.getContext());
-        booksRV.setLayoutManager(myManager);
+        loadBooksBar = view.findViewById(R.id.loadBooksBar);
+        loadBooksBar.setVisibility(View.VISIBLE);
 
-        detector = new GestureDetectorCompat(view.getContext(), new RecyclerViewOnGestureListener());
 
-        booksRV.addOnItemTouchListener(new RecyclerView.SimpleOnItemTouchListener() {
-            @Override
-            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
-                return detector.onTouchEvent(e);
-            }
-        });
+        db = FirebaseFirestore.getInstance();
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setTimestampsInSnapshotsEnabled(true)
+                .build();
+        db.setFirestoreSettings(settings);
+
+        booksArray = new ArrayList<>();
+
+
+        db.collection("books").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Map<String, Object> book = document.getData();
+                                booksArray.add(new BooksModel.BooksInfo(
+                                        book.get("title").toString(),
+                                        book.get("author").toString(),
+                                        book.get("genre").toString(),
+                                        book.get("description").toString(),
+                                        book.get("publisher").toString(),
+                                        book.get("language").toString(),
+                                        book.get("available").toString()
+                                ));
+                            }
+
+                            // recycler view code.
+                            loadBooksBar.setVisibility(View.GONE);
+                            booksAdapter = new BooksAdapter(booksArray);
+                            booksRV = view.findViewById(R.id.myRV);
+                            booksRV.setAdapter(booksAdapter);
+
+                            bookDescriptionFragment = new BookDescriptionFragment();
+                            RecyclerView.LayoutManager myManager = new LinearLayoutManager(view.getContext());
+                            booksRV.setLayoutManager(myManager);
+
+                            detector = new GestureDetectorCompat(view.getContext(), new RecyclerViewOnGestureListener());
+
+                            booksRV.addOnItemTouchListener(new RecyclerView.SimpleOnItemTouchListener() {
+                                @Override
+                                public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+                                    return detector.onTouchEvent(e);
+                                }
+                            });
+
+                        } else {
+                            Log.w("ListAllBooks", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
 
         return view;
     }
