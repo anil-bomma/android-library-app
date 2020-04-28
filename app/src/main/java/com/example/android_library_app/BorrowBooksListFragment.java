7b494ef6f16ce19d2ec4e78fe2ba612715837cb9
bomma.anil <anil.bommaa@gmail.com>
2020-04-27 21:25:38 -0500

    added progress bar in borrowed book fragments xml and java file

--- app/src/main/java/com/example/android_library_app/BorrowBooksListFragment.java
+++ app/src/main/java/com/example/android_library_app/BorrowBooksListFragment.java
@@ -7,11 +7,9 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,22 +19,18 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
 class BorrowedBooksModel {
-
-    private FirebaseFirestore db;
 
     public static class BorrowedBooksInfo {
         public String borrowedBookName;
@@ -56,46 +50,16 @@ class BorrowedBooksModel {
 
     private static BorrowedBooksModel singleton = null;
 
-    public static BorrowedBooksModel getSingleton() {
-        if (singleton == null) {
-            singleton = new BorrowedBooksModel();
-        }
+    public static BorrowedBooksModel getSingleton(ArrayList<BorrowedBooksInfo> booksArray) {
+        singleton = new BorrowedBooksModel(booksArray);
         return singleton;
     }
 
     public ArrayList<BorrowedBooksInfo> borrowedbooksArray;
 
-    private BorrowedBooksModel() {
-        borrowedbooksArray = new ArrayList<>();
-        db = FirebaseFirestore.getInstance();
-        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
-                .setTimestampsInSnapshotsEnabled(true)
-                .build();
-        db.setFirestoreSettings(settings);
-        loadModel();
-    }
+    private BorrowedBooksModel(ArrayList<BorrowedBooksInfo> booksArray) {
+        borrowedbooksArray = booksArray;
 
-    public void loadModel() {
-        System.out.println("------------load model-----------");
-        db.collection("borrowedBooks").get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Map<String, Object> borrowedBook = document.getData();
-                                borrowedbooksArray.add(new BorrowedBooksInfo(
-                                        borrowedBook.get("bookName").toString(),
-                                        borrowedBook.get("userID").toString(),
-                                        borrowedBook.get("borrowDate").toString(),
-                                        borrowedBook.get("returnDate").toString()
-                                ));
-                            }
-                        } else {
-                            Log.w("StudentBorrowedBookList", "Error getting documents.", task.getException());
-                        }
-                    }
-                });
     }
 }
 
@@ -112,9 +76,9 @@ class BorrowedBooksAdapter extends RecyclerView.Adapter<BorrowedBooksAdapter.Bor
 
     BorrowedBooksModel borrowedBooksModel;
 
-    public BorrowedBooksAdapter() {
+    public BorrowedBooksAdapter(ArrayList<BorrowedBooksModel.BorrowedBooksInfo> borrowedBooks) {
         super();
-        borrowedBooksModel = BorrowedBooksModel.getSingleton();
+        borrowedBooksModel = BorrowedBooksModel.getSingleton(borrowedBooks);
     }
 
     @NonNull
@@ -144,9 +108,14 @@ class BorrowedBooksAdapter extends RecyclerView.Adapter<BorrowedBooksAdapter.Bor
 
 public class BorrowBooksListFragment extends Fragment {
 
+    private FirebaseFirestore db;
+
     private BorrowedBooksAdapter borrowedbooksAdapter = null;
+    private ArrayList<BorrowedBooksModel.BorrowedBooksInfo> borrowedBooksList;
     private RecyclerView borrowedBookRV = null;
     private GestureDetectorCompat detector = null;
+    ProgressBar borrowBookPB;
+    TextView errorTV;
 
     private class RecyclerViewOnGestureListener extends GestureDetector.SimpleOnGestureListener {
 
@@ -160,7 +129,8 @@ public class BorrowBooksListFragment extends Fragment {
                     int position = holder.getAdapterPosition();
 
                     //Obtaining the title of the book
-                    BorrowedBooksModel myModel = BorrowedBooksModel.getSingleton();
+                    BorrowedBooksModel myModel = BorrowedBooksModel.getSingleton(
+                            new ArrayList<BorrowedBooksModel.BorrowedBooksInfo>());
 
                     // handling single tap.
                     Log.d("click", "clicked on item " + position);
@@ -175,19 +145,66 @@ public class BorrowBooksListFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_borrowed_books_list, container, false);
-        // recycler view code.
-        borrowedbooksAdapter = new BorrowedBooksAdapter();
-        borrowedBookRV = view.findViewById(R.id.borrowedBookRV);
-        borrowedBookRV.setAdapter(borrowedbooksAdapter);
-        RecyclerView.LayoutManager myManager = new LinearLayoutManager(view.getContext());
-        borrowedBookRV.setLayoutManager(myManager);
-        detector = new GestureDetectorCompat(view.getContext(), new BorrowBooksListFragment.RecyclerViewOnGestureListener());
-        borrowedBookRV.addOnItemTouchListener(new RecyclerView.SimpleOnItemTouchListener() {
-            @Override
-            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
-                return detector.onTouchEvent(e);
-            }
-        });
+
+        db = FirebaseFirestore.getInstance();
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setTimestampsInSnapshotsEnabled(true)
+                .build();
+        db.setFirestoreSettings(settings);
+
+        borrowBookPB = view.findViewById(R.id.borrowBookPB);
+        errorTV = view.findViewById(R.id.errorTV);
+
+        borrowedBooksList = new ArrayList<>();
+        Query collection;
+        if (MainActivity.userRole.equalsIgnoreCase("student")) {
+            collection = db.collection("borrowedBooks")
+                    .whereEqualTo("userID", MainActivity.user919);
+        } else {
+            collection = db.collection("borrowedBooks");
+        }
+        collection.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Map<String, Object> borrowedBook = document.getData();
+                                borrowedBooksList.add(new BorrowedBooksModel.BorrowedBooksInfo(
+                                        borrowedBook.get("bookName").toString(),
+                                        borrowedBook.get("userID").toString(),
+                                        borrowedBook.get("borrowDate").toString(),
+                                        borrowedBook.get("returnDate").toString()
+                                ));
+                            }
+
+                            if (borrowedBooksList.size() < 1) {
+                                errorTV.setText("You have not borrowed any books yet...!");
+                                errorTV.setVisibility(View.VISIBLE);
+                            }
+
+                            // recycler view code.
+                            borrowedbooksAdapter = new BorrowedBooksAdapter(borrowedBooksList);
+                            borrowedBookRV = view.findViewById(R.id.borrowedBookRV);
+                            borrowedBookRV.setAdapter(borrowedbooksAdapter);
+                            borrowBookPB.setVisibility(View.GONE);
+
+                            RecyclerView.LayoutManager myManager = new LinearLayoutManager(view.getContext());
+                            borrowedBookRV.setLayoutManager(myManager);
+                            detector = new GestureDetectorCompat(view.getContext(), new BorrowBooksListFragment.RecyclerViewOnGestureListener());
+                            borrowedBookRV.addOnItemTouchListener(new RecyclerView.SimpleOnItemTouchListener() {
+                                @Override
+                                public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+                                    return detector.onTouchEvent(e);
+                                }
+                            });
+                        } else {
+                            borrowBookPB.setVisibility(View.GONE);
+                            Log.w("StudentBorrowedBookList", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
         return view;
     }
 }
